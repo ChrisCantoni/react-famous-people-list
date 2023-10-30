a4c9bb5571b89334836fd5ce93c49f7839d5ea12
Chris Cantoni <chris.cantoni@gmail.com>
2023-10-30 12:38:30 -0500

    POST request added and DOM updated, inputs cleared.

--- src/components/FamousSection/FamousSection.jsx
+++ src/components/FamousSection/FamousSection.jsx
@@ -28,7 +28,15 @@ function FamousSection() {
     console.log(`The person is ${famousPersonName} and they're famous for ${famousPersonRole}`);
     
     // TODO: create POST request to add this new person to the database
-
+    axios.post('/people', {
+      name: famousPersonName,
+      role: famousPersonRole
+    }).then((response) => {
+      console.log('POST to fame was successful');
+      setPersonName('');
+      setPersonRole('');
+      fetchPeople();
+    })
     // HINT: the server is expecting a person object 
     //       with a `name` and a `role` property
   
@@ -38,16 +46,18 @@ function FamousSection() {
       <section className="new-person-section">
         <form onSubmit={addPerson}>
           <label htmlFor="name-input">Name:</label>
-          <input id="name-input" onChange={e => setPersonName(e.target.value)} />
+          <input value={famousPersonName} id="name-input" onChange={e => setPersonName(e.target.value)} />
           <label htmlFor="role-input">Famous for:</label>
-          <input id="role-input" onChange={e => setPersonRole(e.target.value)} />
+          <input value={famousPersonRole} id="role-input" onChange={e => setPersonRole(e.target.value)} />
           <button type="submit">Done</button>
         </form>
         <p>
           {famousPersonName} is famous for "{famousPersonRole}".
         </p>
         <ul>
-          {/* TODO: Render the list of famous people */}
+          {famousPeopleArray.map((person) => {
+            return <li>{person.name} {person.role}</li>
+          })}
         </ul>
       </section>
     );
